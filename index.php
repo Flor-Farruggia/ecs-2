b2c2e438d81d01084fd2e59ce722690ee4824770
FlorFarruggia <flor.farru@gmail.com>
2023-05-19 22:26:21 -0300

    Ecs-2: create files and do excersice functional.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'models/userData.php';
+
+$dp = new dataUser ();
+$dp -> id = "1";
+$dp -> userName = "FlorFarru";
+$dp -> key = "g1html56";
+$dp -> email = "ecs2@exam.com";
+$dp -> name = "Florencia";
+$dp -> lastName = "Farruggia";
+$dp -> birthDate = "10-10-2010";
+$dp -> document = "10333444";
+$dp -> discharge = "30-03-2023";
+
+
+$dp -> allDataUser ();
---
+++ models/userData.php
@@ -0,0 +1,35 @@
+<?php
+/**Declarar una clase “Usuario” con las siguientes propiedades: 
+Id
+NombreUsuario
+Clave
+Email
+Nombre
+Apellido
+FechaNacimiento
+NroDocumento
+FechaAlta
+ */
+class dataUser {
+    public $id;
+    public $userName;
+    public $key;
+    public $email;
+    public $name;
+    public $lastName;
+    public $birthDate;
+    public $document;
+    public $discharge;
+
+    public function allDataUser (){
+        echo 'Id: '.$this->id;
+        echo '<br> User name: '.$this->userName;
+        echo '<br> Key: '.$this->key;
+        echo '<br> Email: '.$this->email;
+        echo '<br> Name: '.$this->name; 
+        echo '<br> Last Name: '.$this->lastName;
+        echo '<br> Birthdate: '.$this->birthDate;
+        echo '<br> Document: '.$this->document;
+        echo '<br> Discharge date: '.$this->discharge;
+    }
+}
